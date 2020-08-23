fc70d9e38e3b036195c2c4fd09410877bfb8728b
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-08-23 13:32:53 +0530

    week 2 problem 4

---
+++ code4.py
@@ -0,0 +1,15 @@
+a, b = list(map(int, input().split()))
+
+
+def gcd(a, b):
+    if (b == 0):
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+print(lcm(a, b))
